de1db4e5fd1572781c85f3e2d46ac7b6e4325aca
chairielazizi <azizihr2000@gmail.com>
2025-01-27 16:48:39 +0800

    change index view, add properties to display icon and title

--- ExpenseTracker/Models/Category.cs
+++ ExpenseTracker/Models/Category.cs
@@ -17,5 +17,10 @@ namespace ExpenseTracker.Models
         [Column(TypeName = "nvarchar(10)")]
         public string TransactionType { get; set; } = "Expense";
 
+        [NotMapped]
+        public string? TitleWithIcon {
+            get { return this.Icon + " " + this.Title; }
+
+        }
     }
 }
