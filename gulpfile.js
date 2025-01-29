5f72355d4c10345f82ab1efc894df9c60a20e0f0
chairielazizi <azizihr2000@gmail.com>
2025-01-29 11:59:05 +0800

    configure scss

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+﻿const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+const paths = {
+    scss: {
+        src: 'ExpenseTracker/wwwroot/scss/**/*.scss',
+        dest: 'ExpenseTracker/wwwroot/css'
+    }
+};
+
+function styles() {
+    return gulp.src(paths.scss.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.scss.dest));
+}
+
+function watch() {
+    gulp.watch(paths.scss.src, styles);
+}
+
+const build = gulp.series(styles, watch);
+
+exports.styles = styles;
+exports.watch = watch;
+exports.default = build;
